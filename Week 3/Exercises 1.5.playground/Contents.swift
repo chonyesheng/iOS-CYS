b1d74ac6d1b159d40edac12da3a51b0dc3e803fe
chonyesheng <chonyesheng1014@e.neivce.edu.my>
2024-11-20 09:46:44 +0800

    Week 3 Exercises 1.5 done

--- Week 3/Exercises 1.5.playground/Contents.swift
+++ Week 3/Exercises 1.5.playground/Contents.swift
@@ -1,3 +1,17 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let username = "ys"
+let password = "1234567"
+
+let inputUsername = "ys"
+let inputPassword = "1234567"
+
+if inputUsername == username {
+    if inputPassword == password {
+        print("Login Successfully!!")
+    }else{
+        print("Invalid Password!!")
+    }
+}else{
+    print("Invalid Username!!")
+}
