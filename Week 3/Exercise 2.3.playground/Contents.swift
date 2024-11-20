335cce07b0bb08c65588471c8b710e49a2e1ae8a
chonyesheng <chonyesheng1014@e.neivce.edu.my>
2024-11-20 10:33:44 +0800

    Week 3 Exercise 2.3 done

--- Week 3/Exercise 2.3.playground/Contents.swift
+++ Week 3/Exercise 2.3.playground/Contents.swift
@@ -1,3 +1,6 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let ItemsInCart = 5
+
+let DiscountMessage = ItemsInCart >= 3 ? "Eligible in Message" : "Not eligible in Message"
+print(DiscountMessage)
