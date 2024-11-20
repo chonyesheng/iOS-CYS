a0d9cbc2c39c73839f306864536e662e109fad17
chonyesheng <chonyesheng1014@e.neivce.edu.my>
2024-11-20 09:41:15 +0800

    Week 3 Exercises 1.4 done

--- Week 3/Exercises 1.4.playground/Contents.swift
+++ Week 3/Exercises 1.4.playground/Contents.swift
@@ -1,3 +1,14 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let age = 30
+
+if age < 5 {
+    print("Free entry")
+}else if age >= 5 && age <= 12 {
+    print("Ticket price is $10")
+}else if age >= 13 && age <= 64 {
+    print("Ticket price is $15")
+}else if age >= 64 && age <= 64 {
+    print("Ticket price is 8")
+}
+
