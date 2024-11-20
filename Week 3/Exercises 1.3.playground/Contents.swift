90f440d595042b5c80696c0850530de116590e8b
chonyesheng <chonyesheng1014@e.neivce.edu.my>
2024-11-20 09:32:30 +0800

    Week 3 Exercises 1.3 done

--- Week 3/Exercises 1.3.playground/Contents.swift
+++ Week 3/Exercises 1.3.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let year = 2024
+
+if year % 4 == 0 {
+    if year % 100 != 0 || year % 400 == 0 {
+        print("Leap Year")
+    }else{
+        print("Not a Leap Year")
+    }
+}else{
+    print("Not a Leap Year")
+    }
