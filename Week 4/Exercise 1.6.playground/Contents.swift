bde9220c43e7a96478cdbed1586298821e00c9fa
chonyesheng <chonyesheng1014@e.neivce.edu.my>
2024-11-27 10:22:52 +0800

    Week 4 Exercise 1.6 done

--- Week 4/Exercise 1.6.playground/Contents.swift
+++ Week 4/Exercise 1.6.playground/Contents.swift
@@ -1,3 +1,14 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func printHalfDiamondPattern() {
+    let n = 4
+    
+    for i in 1...n {
+        print(String(repeating: "*", count: i))
+    }
+    for i in (1..<n).reversed() {
+        print(String(repeating: "*", count: i))
+    }
+}
+
+printHalfDiamondPattern()
