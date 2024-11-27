ccaf10824f507442b147f53db97e285b0f8c812b
chonyesheng <chonyesheng1014@e.neivce.edu.my>
2024-11-27 09:40:33 +0800

    done 1.1

--- Week 4/Week 4 Exercise 1.2.playground/Contents.swift
+++ Week 4/Week 4 Exercise 1.2.playground/Contents.swift
@@ -1,3 +1,14 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let number = 2534
+let numberString = String(number)
+
+var sum = 0
+
+for char in numberString {
+    if let digit = Int(String(char)) {
+        sum += digit
+    }
+}
+
+print("The sum of the digits of \(number) is \(sum)")
