4e63090b33f593c79a171234367ff9cf94a9febf
chonyesheng <chonyesheng1014@e.neivce.edu.my>
2024-11-27 10:02:52 +0800

    Week 4 Exercise 1.4  done

--- Week 4/Exercise 1.4.playground/Contents.swift
+++ Week 4/Exercise 1.4.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
-var greeting = "Hello, playground"
+for number in 1...20 {
+    if number % 3 == 0 && number % 5 == 0 {
+        print("FizzBuzz")
+    } else if number % 3 == 0 {
+        print("Fizz")
+    } else if number % 5 == 0 {
+        print("Buzz")
+    } else {
+        print(number)
+    }
+}
