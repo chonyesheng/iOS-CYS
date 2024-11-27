c16b6c091be8255d603622facb76580978599624
chonyesheng <chonyesheng1014@e.neivce.edu.my>
2024-11-27 09:50:26 +0800

    Week 4 Exercise 1.3 done

--- Week 4/Week 4 Exercise 1.3.playground/Contents.swift
+++ Week 4/Week 4 Exercise 1.3.playground/Contents.swift
@@ -1,3 +1,18 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let numbers = [12, 45, 2, 67, 33, 8, 99, 23]
+
+var smallest = numbers[0]
+var largest = numbers[0]
+
+for number in numbers {
+    if number < smallest {
+        smallest = number
+    }
+    if number > largest {
+        largest = number
+    }
+}
+
+print("The smallest is \(smallest) ")
+print("The largest is \(largest)")
