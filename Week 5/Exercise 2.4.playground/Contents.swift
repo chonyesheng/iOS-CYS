7c16d55df3d15b991e668d1dbea88c613c5a7655
chonyesheng <chonyesheng1014@e.neivce.edu.my>
2024-12-04 11:20:31 +0800

    Week 5 Exercsie 2.4

--- Week 5/Exercise 2.4.playground/Contents.swift
+++ Week 5/Exercise 2.4.playground/Contents.swift
@@ -1,3 +1,18 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func factorial(n: Int) -> Int {
+    if n < 0 {
+        print("factional is not defined for negative number.")
+        return -1
+    }
+    
+    var result = 1
+    for i in 1...n {
+        result *= i
+    }
+    
+    return result
+}
+
+let number = 5
+
