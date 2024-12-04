389b514ab61456f03c8a527618c292f321b86345
chonyesheng <chonyesheng1014@e.neivce.edu.my>
2024-12-04 09:51:30 +0800

    Edit week 4 Exercise 1.3

--- Week 4/Week 4 Exercise 1.3.playground/Contents.swift
+++ Week 4/Week 4 Exercise 1.3.playground/Contents.swift
@@ -1,18 +1,30 @@
 import UIKit
 
-let numbers = [12, 45, 2, 67, 33, 8, 99, 23]
-
-var smallest = numbers[0]
-var largest = numbers[0]
-
-for number in numbers {
-    if number < smallest {
-        smallest = number
+func findMinAndMax(in numbers: [Int]) -> (smallest: Int, largest: Int)? {
+    guard !numbers.isEmpty else { return nil }
+    
+    var smallest = numbers[0]
+    var largest = numbers[0]
+    
+    for number in numbers {
+        if number < smallest {
+            smallest = number
+        }
+        if number > largest {
+            largest = number
+        }
     }
-    if number > largest {
-        largest = number
-    }
+    
+    return (smallest, largest)
 }
+    
+    let numbers = [12, 45, 2, 67, 33, 8, 99, 223]
+    
+    if let result = findMinAndMax(in: numbers) {
+        print("The smallest is \(result.smallest) ")
+        print("The largest is \(result.largest)")
+    } else {
+        print("The numbers is empty")
+    }
 
-print("The smallest is \(smallest) ")
-print("The largest is \(largest)")
+
