a71780d89c5f893769cbef67cba08ad6754a188d
chonyesheng <chonyesheng1014@e.neivce.edu.my>
2024-12-04 10:59:26 +0800

    Week 5 Exercise 2.2 done

--- Week 5/Exercise 2.2.playground/Contents.swift
+++ Week 5/Exercise 2.2.playground/Contents.swift
@@ -1,3 +1,24 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func countEvenOdd(start: Int, end: Int) -> (evenCount: Int, OddCount: Int) {
+    
+    var evenCount = 0
+    var OddCount = 0
+    
+    for number in start...end {
+        if number % 2 == 0 {
+            evenCount += 1
+        } else {
+            oddCount += 1
+        }
+    }
+    
+    return (evenCount, OddCount)
+}
+
+let startNumber = 1
+let endNumber = 10
+let (evenCount, oddCount) = countEvenOdd (start: startNumber, end: endNumber)
+
+print("Even numbers: \(evenCount), Odd numbers \(oddCount)")
+
