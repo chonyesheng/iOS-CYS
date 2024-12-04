a28cdcc2532dccbd3228f2e8f22fff666c862ddf
chonyesheng <chonyesheng1014@e.neivce.edu.my>
2024-12-04 11:11:13 +0800

    Week 5 Exercise 2.3 done

--- Week 5/Exercise 2.3.playground/Contents.swift
+++ Week 5/Exercise 2.3.playground/Contents.swift
@@ -1,3 +1,16 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func multiplicationTable(n : Int) {
+    if n < 0 {
+        print("Please enter a positive number")
+    } else {
+        for i in 1...10 {
+            let result = n * 1
+            
+            print("\(n) * \(i) = \(result)")
+        }
+    }
+}
+
+let number = 5
+multiplicationTable(n : number)
