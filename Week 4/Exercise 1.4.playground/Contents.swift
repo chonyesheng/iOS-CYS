3da69e4c3f3476b6b87981600ae3daed2fafbbff
chonyesheng <chonyesheng1014@e.neivce.edu.my>
2024-12-04 10:17:03 +0800

    Edit Week 4 Exercise 1.4

--- Week 4/Exercise 1.4.playground/Contents.swift
+++ Week 4/Exercise 1.4.playground/Contents.swift
@@ -1,13 +1,26 @@
 import UIKit
 
-for number in 1...20 {
-    if number % 3 == 0 && number % 5 == 0 {
-        print("FizzBuzz")
-    } else if number % 3 == 0 {
-        print("Fizz")
-    } else if number % 5 == 0 {
-        print("Buzz")
-    } else {
-        print(number)
+func fizzBuzz(from start: Int, to end: Int) -> [String] {
+    var result: [String] = []
+    
+    
+    for number in start...end {
+        if number % 3 == 0 && number % 5 == 0 {
+            result.append("FizzBuzz")
+        } else if number % 3 == 0 {
+            result.append("Fizz")
+        } else if number % 5 == 0 {
+            result.append("Buzz")
+        } else {
+            result.append("\number)")
+        }
     }
+    
+    return result
 }
+
+let fizzBuzzResult = fizzBuzz(from: 1 ,to: 20)
+    for output in fizzBuzzResult {
+    print(output)
+}
+            
