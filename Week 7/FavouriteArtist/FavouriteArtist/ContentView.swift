5f39d7b4df941751b01cf41e4a97c1d053063762
chonb ye sheng <chonyesheng1014@e.neivce.edu.my>
2024-12-18 11:26:38 +0800

    done week 7

--- Week 7/FavouriteArtist/FavouriteArtist/ContentView.swift
+++ Week 7/FavouriteArtist/FavouriteArtist/ContentView.swift
@@ -10,15 +10,92 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            HStack {
+                Spacer()
+                Image(systemName: "bell.badge.fill")
+                    .padding(.all)
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                
+                
+                Image(systemName: "person.fill")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+            }
         }
         .padding()
-    }
-}
-
+        
+        
+        
+        HStack {
+            Text("Favourite Artist")
+                .fontWeight(.semibold)
+                .font(.largeTitle)
+            Spacer()
+        }
+        
+        HStack {
+            Text("ROSE")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+            
+            Image("ROSE")
+                .resizable()
+                .scaledToFill()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(20)
+            Spacer()
+        }.background(.pink)
+            
+            
+            HStack {
+                Text("YANGMI")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                
+                Image("YANGMI")
+                    .resizable()
+                    .scaledToFill()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.all)
+                    .cornerRadius(20)
+                Spacer()
+            }.background(.blue)
+                
+                
+                
+        HStack {
+            Text("GEM")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+            
+            Image("GEM")
+                .resizable()
+                .scaledToFill()
+                .aspectRatio(contentMode: .fit)
+                .padding(.all)
+                .cornerRadius(20)
+            Spacer()
+        }.background(.yellow)
+        
+        
+        HStack {
+            
+            Text("ZHAO LUSI")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+            
+            Image("ZHAO LUSI")
+                .resizable()
+                .scaledToFill()
+                .aspectRatio(contentMode: .fit)
+                .padding(.all)
+                .cornerRadius(20)
+            Spacer()
+        }.background(.red)
+                }
+            }
 #Preview {
     ContentView()
 }
