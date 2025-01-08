1c280eae1f4e0da4b88babe754c26f95d35c8829
chonb ye sheng <chonyesheng1014@e.neivce.edu.my>
2025-01-08 11:34:21 +0800

    EXERCISE 1.2 WEEK 8 DONE

--- Week 8/Exercise 1.2 (Week 8)/Exercise 1.2 (Week 8)/weather.swift
+++ Week 8/Exercise 1.2 (Week 8)/Exercise 1.2 (Week 8)/weather.swift
@@ -7,12 +7,41 @@
 
 import SwiftUI
 
-struct weather: View {
+struct Weather: View {
+var name: String
+var image: String
+var tempature: String
+
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 10) {
+            Text(name)
+                .font(.title)
+                .fontWeight(.bold)
+            
+            Image(systemName: image)
+                .symbolRenderingMode(.multicolor)
+                .font(.largeTitle)
+                .foregroundStyle(.tint)
+            
+            Text(tempature)
+                .font(.title2)
+                .fontWeight(.semibold)
+            
+        }
+        .padding()
+        .background(Color(.systemBackground))
+        .cornerRadius(10)
+        .shadow(radius: 5)
     }
 }
 
 #Preview {
-    weather()
+    Weather(name: "MON", image: "moon.stars", tempature: "33C")
+    Weather(name: "TUE", image: "moon.stars", tempature: "33C")
+    Weather(name: "WED", image: "moon.stars", tempature: "33C")
+    Weather(name: "THU", image: "moon.stars", tempature: "33C")
+    Weather(name: "FRI", image: "moon.stars", tempature: "33C")
+    Weather(name: "MON", image: "moon.stars", tempature: "33C")
+        
 }
