f7d939e7d8fd74294f12292086e31fe25975df35
chonb ye sheng <chonyesheng1014@e.neivce.edu.my>
2025-01-15 11:31:58 +0800

    Week 9

---
+++ Week 9/Fruit/Fruit/FruitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitApp.swift
+//  Fruit
+//
+//  Created by labuser on 15/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct FruitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
