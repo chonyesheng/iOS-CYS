d257d57641287bdfe7918df91595ff547f2d84df
chonb ye sheng <chonyesheng1014@e.neivce.edu.my>
2025-01-22 12:05:53 +0800

    EDIT Week 12

--- Week 12/NavigationStack/NavigationStack/ContentView.swift
+++ Week 12/NavigationStack/NavigationStack/ContentView.swift
@@ -14,51 +14,71 @@ struct Weather: Identifiable {
 }
 
 struct ContentView: View {
-    let asia = ["Tokyo", "Singapore", "Shang Hai"]
+    let asia = ["Tokyo", "Singapore", "Shanghai"]
     let europe = ["New York", "London", "Canada"]
+    
+    @State private var searchText = ""
+    
     @State private var isSheetPresented = false
+    
+    var filteredAsia: [String] {
+        if searchText.isEmpty {
+            return asia
+        } else {
+            return asia.filter { $0.localizedCaseInsensitiveContains(searchText) }
+        }
+    }
+    
+    var filteredEurope: [String] {
+        if searchText.isEmpty {
+            return europe
+        } else {
+            return europe.filter { $0.localizedCaseInsensitiveContains(searchText) }
+        }
+    }
     
     var body: some View {
         NavigationStack {
-            List {
-                Section(header: Text("Asia")) {
-                    ForEach(asia, id: \.self) { asia in
-                        NavigationLink {
-                            Text("Item's Detail \(asia)")
-                        } label: {
-                            Text(asia)
+            VStack {
+                List {
+                    Section(header: Text("Asia")) {
+                        ForEach(filteredAsia, id: \.self) { city in
+                            NavigationLink {
+                                WeatherDetailsView(city: city)
+                            } label: {
+                                Text(city)
+                            }
+                        }
+                    }
+                    
+                    Section(header: Text("Europe")) {
+                        ForEach(filteredEurope, id: \.self) { city in
+                            NavigationLink {
+                                WeatherDetailsView(city: city)
+                            } label: {
+                                Text(city)
+                            }
                         }
                     }
                 }
-                
-                Section(header: Text("Europe")) {
-                    ForEach(europe, id: \.self) { europe in
-                        NavigationLink {
-                            Text("Item's Detail \(europe)")
-                        } label: {
-                            Text(europe)
+                .searchable(text: $searchText)
+                .navigationTitle("Cities")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button(action: {
+                            isSheetPresented = true
+                        }) {
+                            Text("+")
+                                .font(.system(size: 50))
                         }
                     }
                 }
-            }
-            .navigationTitle("Cities")
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button(action: {
-                        isSheetPresented = true
-                        
-                    }){
-                        
-                        Text("+")
-                            .font(.system(size: 50))
-                    }
+                .sheet(isPresented: $isSheetPresented) {
+                    Text("This application is created by Chon Ye Sheng")
+                        .presentationDetents([.height(500), .large])
+                        .font(.headline)
+                        .padding()
                 }
-            }
-            .sheet(isPresented: $isSheetPresented) {
-                Text("This application is created by Chon Ye Sheng")
-                    .presentationDetents([.height(300), .large])
-                    .font(.headline)
-                    .padding()
             }
         }
     }
@@ -68,13 +88,13 @@ struct WeatherDetailsView: View {
     let city: String
     
     let forecast: [Weather] = [
-        Weather(day: "Monday", tempature: "25C"),
-        Weather(day: "Tuesday", tempature: "27C"),
-        Weather(day: "Wednesday", tempature: "33C"),
-        Weather(day: "Thursday", tempature: "20C"),
-        Weather(day: "Friday", tempature: "25C"),
-        Weather(day: "Saturday", tempature: "23C"),
-        Weather(day: "Sunday", tempature: "23C")
+        Weather(day: "Monday", tempature: "25°C"),
+        Weather(day: "Tuesday", tempature: "27°C"),
+        Weather(day: "Wednesday", tempature: "33°C"),
+        Weather(day: "Thursday", tempature: "20°C"),
+        Weather(day: "Friday", tempature: "25°C"),
+        Weather(day: "Saturday", tempature: "23°C"),
+        Weather(day: "Sunday", tempature: "23°C")
     ]
     
     var body: some View {
@@ -98,6 +118,8 @@ struct WeatherDetailsView: View {
     }
 }
 
+
 #Preview {
     ContentView()
 }
+
